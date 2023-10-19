fddc2e469e95b1665516f5c4ba68e29b278c1b93
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-19 23:34:50 +0300

    add Event mediator profile to map input object to another

---
+++ GloboTicket.Application/AutoMapper/Profiles/EventProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GloboTicket.Application.Features.Event.Commands;
+using GloboTicket.Application.Models;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventsListDto>().ReverseMap();
+            CreateMap<Event, UpdateEventCommand>().ReverseMap();
+
+        }
+    }
+}
