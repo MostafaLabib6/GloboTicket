09a28d95d826306081671fefe422003fe7c5abf5
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-20 09:56:21 +0300

    add Event Dtos for creation and retriveral

---
+++ GloboTicket.Application/Models/EventDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Application.Models
+{
+    public class EventDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public string? Artist { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
