57d614650bd09b3dbc40f74e8d77850a48025ae7
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-21 17:46:07 +0300

    add Cqrs for order

---
+++ GloboTicket.Application/Features/Order/Query/GetOrderListQuery.cs
@@ -0,0 +1,16 @@
+﻿using GloboTicket.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Application.Features.Order.Query
+{
+    public class GetOrderListQuery : IRequest<List<GetCategoryDto>>
+    {
+    }
+
+
+}
---
+++ GloboTicket.Application/Features/Order/Query/GetOrdersListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using GloboTicket.Application.Contracts.Persistance;
+using GloboTicket.Application.Features.Category.Queries;
+using GloboTicket.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Application.Features.Order.Query
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrderListQuery, List<GetCategoryDto>>
+    {
+        public IMapper _mapper { get; }
+        public ICategoryRepository _repository { get; }
+        public GetOrdersListQueryHandler(IMapper mapper, ICategoryRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+        public async Task<List<GetCategoryDto>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+        {
+            var entiry = await _repository.GetAll();
+
+            return _mapper.Map<List<GetCategoryDto>>(entiry);
+        }
+    }
+}
