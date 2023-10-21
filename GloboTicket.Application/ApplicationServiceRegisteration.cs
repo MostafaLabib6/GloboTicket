eea9d407f7c7605015f60a3b9964dac5d17586b7
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-21 17:47:49 +0300

    Resgister Application services

---
+++ GloboTicket.Application/ApplicationServiceRegisteration.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.Application
+{
+    public static class ApplicationServiceRegisteration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+            return services;
+        }
+    }
+}
