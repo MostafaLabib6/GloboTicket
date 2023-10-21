a4862367fbb020d7abdb039d7a817d49fc59ad3d
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-21 17:47:31 +0300

    add Layer of daitals for Responses by anding proprities to check if Creation Success or not and Liist of errors

---
+++ GloboTicket.Application/Responses/BaseResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace GloboTicket.Application.Responses;
+/// <summary>
+/// BaseResponsse 
+/// Created for if i want return maeningful respone for user rather than 
+/// return guid in Createion as example.
+/// so we can return List of errors and Success or not. 
+///
+/// </summary>
+public class BaseResponse
+{
+
+    public bool Success { get; set; }
+    public List<string> ErrorList { get; set; } = new List<string>();
+    public string Message { get; set; } = string.Empty;
+
+
+    public BaseResponse()
+    {
+        Success = true;
+    }
+    public BaseResponse(bool success, string message)
+    {
+        Success = success;
+        Message = message;
+    }
+}
