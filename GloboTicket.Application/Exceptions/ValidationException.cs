fca7b85606af0c004a402b12729d501161d1edc0
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-21 17:45:03 +0300

    add Custome Validation Excetions

---
+++ GloboTicket.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,16 @@
+﻿
+using FluentValidation.Results;
+namespace GloboTicket.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IEnumerable<string> ErrorList { get; } = new List<string>();
+        public ValidationException(ValidationResult validationResult)
+        {
+            foreach (var exception in validationResult.Errors)
+            {
+                ErrorList.Append(exception.ErrorMessage);
+            }
+        }
+    }
+}
