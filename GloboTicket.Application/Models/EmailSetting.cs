92700e047d9c58398b2b22c8250b123f9a63d5b7
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-22 19:51:33 +0300

    add email models

---
+++ GloboTicket.Application/Models/EmailSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace GloboTicket.Application.Models;
+
+public class EmailSetting
+{
+    public string? ApiKey { get; set; } = string.Empty;
+    public string? FromName { get; set; } = string.Empty;
+    public string? FromEmail { get; set; } = string.Empty;
+}
