7e96fd7ed2f2e54dd16597e22c19c991a2fc72ab
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-22 19:51:20 +0300

    add email service

---
+++ GloboTicket.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using GloboTicket.Application.Models;
+
+namespace GloboTicket.Application.Contracts.Infrastructure;
+
+public interface IEmailService
+{
+    public Task<bool> SendEmail(Email email);
+}
