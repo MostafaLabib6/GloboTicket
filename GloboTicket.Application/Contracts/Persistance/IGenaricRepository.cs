3a042602330944bf9810d9dc5ccf79903e1ea3d1
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-24 18:24:50 +0300

    update genaric repository

--- GloboTicket.Application/Contracts/Persistance/IGenaricRepository.cs
+++ GloboTicket.Application/Contracts/Persistance/IGenaricRepository.cs
@@ -7,12 +7,12 @@ namespace GloboTicket.Application.Contracts.Persistance;
 public interface IGenaricRepository<T> where T : class
 {
     public Task<IReadOnlyList<T>> GetAll();
-    
-    public Task<T> Get(Expression<Func<T,bool>> filter);
-    public Task<T> Get(Guid Id);
-    public Task<T> Add(T entity);
-    public Task Update(T entity); 
 
+    public Task<T?> Get(Expression<Func<T, bool>> filter);
+    public Task<T?> Get(Guid Id);
+    public Task<T> Add(T entity);
+    public Task Update(T entity);
     public Task Delete(T entity);
+    //public Task<IReadOnlyList<T>> GetPagedReponseAsync(int page, int size);
 
 }
