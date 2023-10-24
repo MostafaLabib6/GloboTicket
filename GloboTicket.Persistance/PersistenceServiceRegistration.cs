aba065c849ea279886e89499895120c17204b175
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-24 13:18:36 +0300

    update Order persistance

---
+++ GloboTicket.Persistance/PersistenceServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using GloboTicket.Application.Contracts.Persistance;
+using GloboTicket.Persistance.Repositories;
+using GloboTicket.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboTicket.TicketManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GloboTicketDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("GloboTicketDefaultConnection")));
+
+            services.AddScoped(typeof(IGenaricRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+    }
+}
--- GloboTicket.Persistance/Repositories/OrderRepository.cs
+++ GloboTicket.Persistance/Repositories/OrderRepository.cs
@@ -1,5 +1,6 @@
 ﻿using GloboTicket.Application.Contracts.Persistance;
 using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,18 @@ namespace GloboTicket.Persistance.Repositories
     {
         public OrderRepository(GloboTicketDbContext dbContext) : base(dbContext)
         {
+        }
+
+
+        public async Task<List<Order>> GetPagedOrdersForMonth(DateTime date, int page, int size)
+        {
+            return await _dbContext.Orders.Where(x => x.OrderPlaced.Month == date.Month && x.OrderPlaced.Year == date.Year)
+                .Skip((page - 1) * size).Take(size).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<int> GetTotalCountOfOrdersForMonth(DateTime date)
+        {
+            return await _dbContext.Orders.CountAsync(x => x.OrderPlaced.Month == date.Month && x.OrderPlaced.Year == date.Year);
         }
     }
 }
