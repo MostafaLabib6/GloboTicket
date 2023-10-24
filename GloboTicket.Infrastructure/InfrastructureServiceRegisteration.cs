4c658bf29116d8aa54f4242c463d2dd64d28aa46
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-24 13:19:14 +0300

    update project infrastructure

---
+++ GloboTicket.Infrastructure/InfrastructureServiceRegisteration.cs
@@ -0,0 +1,19 @@
+﻿using GloboTicket.Application.Contracts.Infrastructure;
+using GloboTicket.Application.Models;
+using GloboTicket.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboTicket.Infrastructure;
+
+public static class InfrastructureServiceRegisteration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var EmailConfiguration = configuration.GetSection("EmailSettings");
+        services.Configure<EmailSetting>(EmailConfiguration);
+        services.AddTransient<IEmailService, EmailService>();
+        return services;
+
+    }
+}
